230e4908cb194ea9bf8a19d9c53f6f4cc2e5767c
Konstantin Chvilyov <chvilyov.k@iri.center>
2026-05-04 10:08:20 +0300

    Использовал when

--- src/main/java/ru/t1/education/CallbackSchedulerImpl.java
+++ src/main/java/ru/t1/education/CallbackSchedulerImpl.java
@@ -3,6 +3,7 @@ package ru.t1.education;
 import java.time.Instant;
 import java.util.Timer;
 import java.util.TimerTask;
+
 interface CallbackScheduler extends AutoCloseable {
     void schedule(Runnable callback, Instant when) throws InterruptedException;
 }
@@ -17,7 +18,7 @@ public class CallbackSchedulerImpl implements CallbackScheduler {
             public void run() {
                 callback.run();
             }
-        }, 2000);
+        }, when.toEpochMilli() - System.currentTimeMillis());
     }
 
     @Override
