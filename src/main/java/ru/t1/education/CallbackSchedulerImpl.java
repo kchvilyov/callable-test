c366ad15f8ddd98f0edf22b996a523ef43b829cd
Konstantin Chvilyov <chvilyov.k@iri.center>
2026-05-04 10:28:14 +0300

    Выделил стык

--- src/main/java/ru/t1/education/CallbackSchedulerImpl.java
+++ src/main/java/ru/t1/education/CallbackSchedulerImpl.java
@@ -4,10 +4,6 @@ import java.time.Instant;
 import java.util.Timer;
 import java.util.TimerTask;
 
-interface CallbackScheduler extends AutoCloseable {
-    void schedule(Runnable callback, Instant when) throws InterruptedException;
-}
-
 public class CallbackSchedulerImpl implements CallbackScheduler {
     @Override
     public void schedule(Runnable callback, Instant when) throws InterruptedException {
